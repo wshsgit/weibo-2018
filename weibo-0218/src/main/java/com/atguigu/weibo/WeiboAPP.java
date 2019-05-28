054047d39604a0c6dc9f681d7a20c05fe3160bdf
wshs <wshs_job@sina.com>
2019-05-28 09:12:45 +0800

    weiboApp

---
+++ weibo-0218/src/main/java/com/atguigu/weibo/WeiboAPP.java
@@ -0,0 +1,38 @@
+package com.atguigu.weibo;
+
+import com.atguigu.weibo.controller.WeiboController;
+
+import java.io.IOException;
+
+public class WeiboAPP {
+
+    private static WeiboController controller = new WeiboController();
+
+    public static void main(String[] args) throws IOException {
+
+        controller.init();
+
+//        controller.publish("1001","happy 6.1");
+//        controller.publish("1001","happy 6.2");
+//        controller.publish("1001","happy 6.3");
+//        controller.publish("1001","happy 6.4");
+//        controller.publish("1001","happy 6.5");
+//
+//        controller.publish("1002","unHappy 6.1");
+//        controller.publish("1002","unHappy 6.2");
+//        controller.publish("1002","unHappy 6.3");
+//        controller.publish("1002","unHappy 6.4");
+//        controller.publish("1002","unHappy 6.5");
+
+//        controller.follow("1003","1001");
+//        controller.follow("1003","1002");
+
+//        controller.unFollow("1003","1001");
+
+//        controller.getAllWeibosByStarId("1002");
+//        controller.getAllWeibosByStarId("1001");
+
+//        controller.getAllWeibosByStarId("1004");
+
+    }
+}
